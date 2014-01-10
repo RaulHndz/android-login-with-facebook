4b2ff54aa4a1aaa7b01c9aaf4bf5ab2fb0e7ffe8
Fher Eduardo <fher.gnome@gmail.com>
2014-01-10 00:25:30 -0600

    merge

--- src/com/example/login_with_facebook/MainActivity.java
+++ src/com/example/login_with_facebook/MainActivity.java
@@ -1,22 +1,65 @@
 package com.example.login_with_facebook;
 
-import android.os.Bundle;
+import java.util.Arrays;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import com.facebook.FacebookException;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.LoginButton.OnErrorListener;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	  @Override
+	  protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+	    
+	    final TextView txtSaludo = (TextView)findViewById(R.id.txtGreeting);
+	    
+	    LoginButton authButton = (LoginButton) findViewById(R.id.login_button);
+	      authButton.setOnErrorListener(new OnErrorListener() {
+	       
+	       @Override
+	       public void onError(FacebookException error) {
+	         
+	       }
+	    });
+	      
+	    authButton.setReadPermissions(Arrays.asList("basic_info","email"));
+	  
+	    authButton.setSessionStatusCallback(new Session.StatusCallback() {
+	         
+	         @Override
+	         public void call(Session session, SessionState state, Exception exception) {
+	          
+	          if (session.isOpened()) {                    
+	                    Request.executeMeRequestAsync(session,
+	                            new Request.GraphUserCallback() {
+	                                @Override
+	                                public void onCompleted(GraphUser user,Response response) {
+	                                    if (user != null) {
+	                                      txtSaludo.setText("!Bienvenido! " + user.getUsername());                                      
+	                                    }
+	                                }
+	                            });
+	          }else if(session.isClosed()) {
+	                  txtSaludo.setText("!Bienvenido!");
+	          }
+	         }
+	        });
+	    
+	  }
+	  
+	  @Override
+	   public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	       Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	   }
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-}
---
+++ src/com/example/login_with_facebook/MainFragment.java
@@ -0,0 +1,35 @@
+package com.example.login_with_facebook;
+
+import com.facebook.SessionState;
+
+import android.os.Bundle;
+import android.service.textservice.SpellCheckerService.Session;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+	
+	private static final String TAG = "MainFragment";
+	
+	
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+	    if (state.isOpened()) {
+	        Log.i(TAG, "Logged in...");
+	    } else if (state.isClosed()) {
+	        Log.i(TAG, "Logged out...");
+	    }
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, 
+	        ViewGroup container, 
+	        Bundle savedInstanceState) {
+	    View view = inflater.inflate(R.layout.activity_main, container, false);
+
+	    return view;
+	}
+
+}
